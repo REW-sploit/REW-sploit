e4f2c3eb9e238d2a7f1d4f9541d3b50c32daa11f
cecio <b38911@gmail.com>
2021-11-18 16:05:31 +0100

    fixed bug in Cobalt Strike Parser

--- modules/emulate_config.py
+++ modules/emulate_config.py
@@ -7,5 +7,19 @@ Common variables for emulation
 
 """
 
+import sys
+
 # Used to track EP and apply Fixups when needed
 entry_point = 0
+
+#
+# Parse also extras folder for additional packages
+#
+
+# Try https://github.com/Sentinel-One/CobaltStrikeParser.git
+cbparser = True
+try:
+    sys.path.append(r'extras/CobaltStrikeParser')
+    from extras.CobaltStrikeParser.parse_beacon_config import *
+except:
+    cbparser = False
